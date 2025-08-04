f996b6c97800d2ee25d3b338a52e1be0f1545ca9
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-08-04 14:33:13 +0530

    Post model modified

---
+++ backend/src/model/postModel.js
@@ -0,0 +1,42 @@
+const mongoose=require('mongoose');
+
+const postSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true,
+    },
+    content:{
+        type:String,
+        default:'',
+    },
+    media:{
+        type:String,
+    },
+    likes:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+    }],
+    comments:[
+        {
+            user:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"user",
+            },
+            text:String,
+            createdAt:{
+                type:Date,
+                default:Date.now,
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    }
+},{
+    timestamps:true,
+});
+
+const Post=mongoose.model("post",postSchema);
+module.exports=Post;
